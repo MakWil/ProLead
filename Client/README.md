556c7113d66a2b3397f46b0826374953b4d18cdb
MakWil <marksutandi@gmail.com>
2025-09-01 11:03:55 +0700

    Replace old UI with Modernize-MUI React app - Updated server to serve React build - Cleaned up old HTML files - Ready for deployment

---
+++ Client/README.md
@@ -0,0 +1,27 @@
+# Mamba – Free Bootstrap 5 Multipage Portfolio Website Template
+
+#### Preview
+
+ - [Demo](https://themewagon.github.io/mamba/)
+
+#### Download
+ - [Download from ThemeWagon](https://themewagon.github.io/Modernize-MUI/)
+ 
+ 
+## Getting Started
+
+Clone from GitHub 
+```
+https://github.com/themewagon/Modernize-MUI.git
+```
+
+## Author
+
+Design and code are completely written by AdminMart's design and development team.  
+
+
+## License
+
+ - Design and Code is Copyright &copy; [AdminMart](https://www.adminmart.com/)
+ - Licensed under [MIT]
+ - Distributed by [ThemeWagon](https://themewagon.com)
---
+++ Server/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const path = require('path');
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Serve static files from the React app build directory
+app.use(express.static(path.join(__dirname, '../Client/dist')));
+
+// Routes
+const usersRouter = require('./src/routes/users');
+app.use('/api/users', usersRouter);
+
+// Catch all handler: send back React's index.html file for any non-API routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../Client/dist/index.html'));
+});
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
